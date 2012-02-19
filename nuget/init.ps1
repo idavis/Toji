aadf421123b155be3c20f4caa6f818ee7c20411d
Ian Davis <ian@innovatian.com>
2012-02-18 21:12:26 -0800

    fixing file encoding which was causing havok on the init script. Updating script to uninstall itself(just the packages folder remnants).

--- nuget/init.ps1
+++ nuget/init.ps1
@@ -1,39 +1,41 @@
-﻿param($installPath, $toolsPath, $package, $project)
+param($installPath, $toolsPath, $package, $project)
 
 $rootPath = (Resolve-Path "$installPath\..\..").Path
 $buildPath = "$rootPath\build"
+
 if(!(Test-Path($buildPath))) { New-Item $buildPath -ItemType Directory | Out-Null }
 $buildPath = (Resolve-Path $buildPath).Path
 
 $buildToolsPath = "$toolsPath\build"
-$folders = Get-ChildItem $buildToolsPath -recurse -force | ?{ $_.PSIsContainer } 
-$folders | % { $newpath=(($_.FullName).Replace($buildToolsPath,$buildPath)); if(!(test-path $newpath)){ New-Item -ItemType Directory -Path "$newpath" -Force } }
+$commandsPath = "$toolsPath\commands"
+
+# if we don't detect nuget, copy it over and bootstrap it.
+$nugetIsAvailable = (@(Get-Command nuget -ErrorAction SilentlyContinue).Length -gt 0)
+if(!$nugetIsAvailable) {
+  $nugets = @(Get-ChildItem "$rootPath\*" -recurse -include NuGet.exe)
+  if ($nugets.Length -gt 0) { 
+    $nugetIsAvailable = $true
+  }
+}
+
+if(!$nugetIsAvailable) {
+  Move-Item "$toolsPath\NuGet.ex_" -Destination "$buildPath\NuGet.exe"
+  & "$buildPath\NuGet.exe"
+  if((Test-Path "$buildPath\nuget.exe.old")) { Remove-Item "$buildPath\nuget.exe.old" -Force }
+}
 
 # copy main scripts
 $files = Get-ChildItem "$buildToolsPath" -recurse -exclude settings.ps1, build.ps1, overrides.ps1 | ? { !$_.PSIsContainer } 
-
 $files | % { $newpath=(($_.FullName).Replace($buildToolsPath,$buildPath)); Copy-Item $_.FullName $newpath -Force }
 
 # copy the build.ps1 and settings.ps1, ignore if they already exist
-
 if(!(Test-Path("$buildPath\settings.ps1"))) { Move-Item  "$buildToolsPath\settings.ps1" -Destination $buildPath }
 if(!(Test-Path("$buildPath\build.ps1"))) { Move-Item  "$buildToolsPath\build.ps1" -Destination $buildPath }
 if(!(Test-Path("$buildPath\overrides.ps1"))) { Move-Item  "$buildToolsPath\overrides.ps1" -Destination $buildPath }
 
-# if we don't detect nuget, copy it over and bootstrap it.
-
-$nugetIsOnPath = (@(Get-Command nuget -ErrorAction SilentlyContinue).Length -gt 0)
-if(!$nugetIsOnPath -and !(Test-Path("$buildPath\NuGet.exe"))) {
-  Move-Item "$toolsPath\NuGet.exe" -Destination $buildPath
-  & "$buildPath\NuGet.exe"
-  if(!(Test-Path "$buildPath\nuget.exe.old")) { Remove-Item "$buildPath\nuget.exe.old" -Force }
-}
-
 # copy the solution level build scripts, ignore if they already exist
-$commandsPath = "$toolsPath\commands"
 if(!(Test-Path("$rootPath\build.ps1"))) { Move-Item  "$commandsPath\build.ps1" -Destination $rootPath }
 if(!(Test-Path("$rootPath\build.cmd"))) { Move-Item  "$commandsPath\build.cmd" -Destination $rootPath }
 
 # remove everything as we have copied it over.
-
-Remove-Item $installPath -Recurse -Force
+Uninstall-Package Toji
